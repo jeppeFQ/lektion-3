bc819e74fc778165c732a98cb7e62d301bc1a817
Jeppe FQ <jeppe.fjeldgaard@gmail.com>
2025-09-23 10:11:21 +0200

    øvelse

---
+++ algoritme-øvelse.py
@@ -0,0 +1,22 @@
+# Opgave 1
+for tal in range(1, 101):
+    if tal % 2 == 0:
+        print(tal)
+
+
+# Opgave 2
+sidelængde = int(input("Indtast et positivt heltal mellem 1 og 200: "))
+areal = sidelængde ** 2
+print(areal)
+
+
+# Opgave 3
+navn = input("Indtast dit navn: ")
+if navn == "Arthur":
+    print("Har du husket dit håndklæde?")
+else:
+    print("Hej " + navn + "!")
+
+
+
+
